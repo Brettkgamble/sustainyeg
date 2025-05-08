56b3c817928f6377e412bb8acc96b0af31a72264
Brett Gamble <brettkgamble@gmail.com>
2025-05-08 09:53:39 -0600

    0005 - Prep for Sustain YEG design guide

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import "@workspace/ui/globals.css";
 
 import { Geist, Geist_Mono } from "next/font/google";
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -20,8 +20,10 @@ const config = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        /* background: "hsl(var(--background))",*/
+        background: "var(--background)",
+       /* foreground: "hsl(var(--foreground))", */
+       foreground: "var(--foreground)",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -78,6 +80,48 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: () => ({
+        sustainyeg: {
+          css: {
+            /* '--tw-prose-body': 'var(--color-pink-800)', */
+            '--tw-prose-body': '#1A1A1A',
+            /* '--tw-prose-headings': 'var(--color-pink-900)', */
+            '--tw-prose-headings': '#1A1A1A',
+            '--tw-prose-lead': 'var(--color-pink-700)',
+            '--tw-prose-links': 'var(--color-pink-900)',
+            '--tw-prose-bold': 'var(--color-pink-900)',
+            '--tw-prose-counters': 'var(--color-pink-600)',
+            '--tw-prose-bullets': 'var(--color-pink-400)',
+            '--tw-prose-hr': 'var(--color-pink-300)',
+            '--tw-prose-quotes': 'var(--color-pink-900)',
+            '--tw-prose-quote-borders': 'var(--color-pink-300)',
+            '--tw-prose-captions': 'var(--color-pink-700)',
+            '--tw-prose-code': 'var(--color-pink-900)',
+            '--tw-prose-pre-code': 'var(--color-pink-100)',
+            '--tw-prose-pre-bg': 'var(--color-pink-900)',
+            '--tw-prose-th-borders': 'var(--color-pink-300)',
+            '--tw-prose-td-borders': 'var(--color-pink-200)',
+            /* '--tw-prose-invert-body': 'var(--color-pink-200)', */
+            '--tw-prose-invert-body': '#1A1A1A',
+            /* '--tw-prose-invert-headings': 'var(--color-white)', */
+            '--tw-prose-invert-headings': '#1A1A1A',
+            '--tw-prose-invert-lead': 'var(--color-pink-300)',
+            '--tw-prose-invert-links': 'var(--color-white)',
+            '--tw-prose-invert-bold': 'var(--color-white)',
+            '--tw-prose-invert-counters': 'var(--color-pink-400)',
+            '--tw-prose-invert-bullets': 'var(--color-pink-600)',
+            '--tw-prose-invert-hr': 'var(--color-pink-700)',
+            '--tw-prose-invert-quotes': 'var(--color-pink-100)',
+            '--tw-prose-invert-quote-borders': 'var(--color-pink-700)',
+            '--tw-prose-invert-captions': 'var(--color-pink-400)',
+            '--tw-prose-invert-code': 'var(--color-white)',
+            '--tw-prose-invert-pre-code': 'var(--color-pink-300)',
+            '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
+            '--tw-prose-invert-th-borders': 'var(--color-pink-600)',
+            '--tw-prose-invert-td-borders': 'var(--color-pink-700)',
+          },
+        },
+      }),
     },
   },
   plugins: [tailwindcssAnimate, tailwindcssTypography],
